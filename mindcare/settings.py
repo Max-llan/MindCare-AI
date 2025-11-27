9a351c378edb6ea8a02b92bd328edafe2401ca37
Max-llan <maximiliano.llanquinao@inacapmail.cl>
2025-11-27 16:02:48 -0300

    Agregar el PostgreSQL

--- mindcare/settings.py
+++ mindcare/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@j!6pse73tein_o#i0g^5ni4zx*5$vbpmc(&em^u2yc4qacp(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mindcare-ai-zb9a.onrender.com','localhost']
 
 
 # Application definition
@@ -80,10 +81,9 @@ WSGI_APPLICATION = 'mindcare.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("mindcare_db_jgd6")
+    )
 }
 
 
