077f65b93278d4a3595312e368b2eca4b84d4ca0
Max-llan <maximiliano.llanquinao@inacapmail.cl>
2025-11-29 12:33:39 -0300

    Avances

--- mindcare/urls.py
+++ mindcare/urls.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include
-from api.views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from django.urls import path
+from api.views import (
+    LoginView,
+    RegistroView,
+    home,
+    analizar_form,
+    EvaluacionEmocionalView,
+    AnalizarTextoView
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/login/', LoginView.as_view()),
-      path("api/registro/", RegistroView.as_view(), name="registro"),
-    path('', home, name='home'),
-    path("analizar", analizar_form, name="analizar"),
-    path('api/evaluaciones/', EvaluacionEmocionalView.as_view()),
-    path("api/analizar-texto/", AnalizarTextoView.as_view(), name="analizar-texto"),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
 
+    # AUTH
+    path('api/login/', LoginView.as_view(), name="login"),
+    path('api/registro/', RegistroView.as_view(), name="registro"),
+
+    # FRONTEND / PÁGINAS
+    path('', home, name='home'),
+    path('analizar', analizar_form, name='analizar'),
+
+    # API
+    path('api/evaluaciones/', EvaluacionEmocionalView.as_view(), name="evaluaciones"),
+    path('api/analizar-texto/', AnalizarTextoView.as_view(), name="analizar-texto"),
+]
